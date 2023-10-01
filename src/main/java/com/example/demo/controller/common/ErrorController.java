a64d3e2654bbdb03ea31dbc1e2e4e30a9a9442e0
daylapvlong <101397074+daylapvlong@users.noreply.github.com>
2023-10-01 14:01:29 +0700

    fix pomxml and system setting

--- src/main/java/com/example/demo/controller/common/ErrorController.java
+++ src/main/java/com/example/demo/controller/common/ErrorController.java
@@ -1,4 +1,4 @@
-package controller.common;
+package com.example.demo.controller.common;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
