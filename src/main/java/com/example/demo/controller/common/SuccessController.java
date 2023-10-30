20887997f531160efa1971f497c8880651f85f74
daylapvlong <101397074+daylapvlong@users.noreply.github.com>
2023-10-31 00:50:25 +0700

    implement pagnation style for admin product list and success page

---
+++ src/main/java/com/example/demo/controller/common/SuccessController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SuccessController {
+    @GetMapping("/success")
+    public String success(){return "/common/success";}
+}
